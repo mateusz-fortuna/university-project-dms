bc220f93293c085b6545bf63b76c39f33f6bf16a
Mateusz Fortuna <mat.fortuna@wp.pl>
2023-01-01 08:24:33 +0100

    feat: Fill the organization page with data

--- src/pages/Organization.tsx
+++ src/pages/Organization.tsx
@@ -1,11 +1,44 @@
 import { FC } from "react";
-import { Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { MainNavigation } from "../layouts/MainNavigation";
+import {
+  Timeline,
+  TimelineConnector,
+  TimelineContent,
+  TimelineItem,
+  TimelineSeparator,
+} from "@mui/lab";
+import Avatar from "@mui/material/Avatar";
 
 export const Organization: FC = () => {
   return (
     <MainNavigation title="Struktura organizacyjna">
-      <Typography>Widoczna struktura organizacyjna firmy</Typography>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="calc(100vh - 64px)"
+      >
+        <Timeline>
+          <TimelineItem>
+            <TimelineSeparator>
+              <Avatar src="/assets/images/kowalski.jpg" alt="Boss" />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              Jan Kowalski <i>(prezes)</i>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineSeparator>
+              <Avatar src="/assets/images/nowak.jpg" alt="Worker" />
+            </TimelineSeparator>
+            <TimelineContent>
+              Marek Nowak <i>(pracownik)</i>
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
+      </Box>
     </MainNavigation>
   );
 };
